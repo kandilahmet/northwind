d333013541a6f4052fd76968463f2c1715693d9a
= <=>
2021-04-23 16:05:55 +0300

    23042021

--- src/app/models/categoryResponseModel.ts
+++ src/app/models/categoryResponseModel.ts
@@ -1,6 +1,6 @@
 import { Category } from "./category";
 import { ResponseModel } from "./responseModel";
 
-export interface categoryResponseModel extends ResponseModel{
+export interface CategoryResponseModel extends ResponseModel{
     data:Category[];
 }
--- src/app/models/productResponseModel.ts
+++ src/app/models/productResponseModel.ts
@@ -1,6 +1,6 @@
 import { Product } from "./product";
 import { ResponseModel } from "./responseModel";
 
-export interface productResponseModel extends ResponseModel{
+export interface ProductResponseModel extends ResponseModel{
     data:Product[];
 }

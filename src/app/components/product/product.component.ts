56665b91e0d6ac69035604c04439a74bd6963f16
= <=>
2021-04-23 16:04:12 +0300

    23042021

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-product',
@@ -7,51 +9,9 @@ import { Product } from 'src/app/models/product';
   styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-  product1: Product = {
-    productId: 1,
-    productName: 'Bardak',
-    categoryId: 1,
-    unitPrice: 5,
-    unitsInStock: 5,
-  };
-  product2: Product = {
-    productId: 2,
-    productName: 'Laptop',
-    categoryId: 1,
-    unitPrice: 5,
-    unitsInStock: 5,
-  };
-  product3: Product = {
-    productId: 3,
-    productName: 'Mouse',
-    categoryId: 1,
-    unitPrice: 5,
-    unitsInStock: 5,
-  };
-  product4: Product = {
-    productId: 4,
-    productName: 'Keyboard',
-    categoryId: 1,
-    unitPrice: 5,
-    unitsInStock: 5,
-  };
-  product5: Product = {
-    productId: 5,
-    productName: 'Camera',
-    categoryId: 1,
-    unitPrice: 5,
-    unitsInStock: 5,
-  };
-
-  products: Product[] = [
-    this.product1,
-    this.product2,
-    this.product3,
-    this.product4,
-    this.product5,
-  ];
-
-  constructor() {}
+  //productResponseModel:ProductResponseModel{}
+  constructor(private httpClient:HttpClient) {}
 
   ngOnInit(): void {}
+
 }
